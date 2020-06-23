abf83aa8548fc7b9c0b3bd8faf1822193a99dd59
epiccoder2 <epiccoder7@gmail.com>
2020-06-23 15:22:49 -0700

    Fixed things

--- events/guild/Message Event Code/Automod.js
+++ events/guild/Message Event Code/Automod.js
@@ -1,7 +1,7 @@
 module.exports = async (bot, message) => {
     // Link blocker
     const inviteLinkRegex = /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li|club)|discordapp\.com\/invite|discord\.com\/invite)\/.+[a-z]/gi;
-    if (inviteLinkRegex.exec(message.content)) {
+    if (inviteLinkRegex.test(message.content)) {
         await message.delete({ timeout: 1000 });
         await message.channel.send(
             `${message.author} **you cannot post links from other servers here!**`,
--- models/warns.js
+++ models/warns.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 // Warns Schema
 const Schema = new mongoose.Schema({
-    Warns: Array,
-    User: String,
-    Guild: String,
+    Warns: mongoose.Schema.Types.Array,
+    User: mongoose.Schema.Types.String,
+    Guild: mongoose.Schema.Types.String,
 });
 
 module.exports = mongoose.model('warns', Schema);
