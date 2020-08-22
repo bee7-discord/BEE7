657cabb934f4cd92b423cd371224b16d6a86aca0
Beatzoid <epiccoder7@gmail.com>
2020-08-22 11:38:44 -0700

    Add purge command

--- src/Structures/Command.js
+++ src/Structures/Command.js
@@ -1,4 +1,18 @@
+const { Permissions } = require("discord.js");
+const BEE7Client = require("./BEE7Client");
+
 module.exports = class Command {
+    /**
+     *
+     * @param {BEE7Client} client - The Discord client
+     * @param {String} name - The name of the commands
+     * @param {Object} options - The options for the command
+     * @param {Array} options.aliases - Aliases for the command
+     * @param {String} options.description - Description of the command
+     * @param {String} options.category - Category of the command
+     * @param {String} options.usage - Usage of the command
+     * @param {Permissions} options.permission - Permission of the command
+     */
     constructor(client, name, options = {}) {
         this.client = client;
         this.name = options.name || name;
---
+++ src/commands/moderation/purge.js
@@ -0,0 +1,64 @@
+const Command = require("../../Structures/Command");
+// eslint-disable-next-line no-unused-vars
+const { Message, MessageEmbed } = require("discord.js");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            name: "purge",
+            description: "Purge some messages",
+            usage: "purge <number>",
+            category: "Moderation",
+            permission: "MANAGE_MESSAGES"
+        });
+    }
+
+    /**
+     *
+     * @param {Message} message - The message of the command
+     * @param {String[]} args - The arguments of the command
+     */
+
+    // eslint-disable-next-line no-unused-vars
+    async run(message, args) {
+        try {
+            if (!args[0]) {
+                return message.channel.send(
+                    "Please give an amount of messages to delete."
+                );
+            }
+
+            BigPurge(args[0]);
+
+            let purgedembed = message.channel.send(
+                new MessageEmbed()
+                    .setTitle(`**Purged**`)
+                    .setDescription(`Deleted ${args[0]} messages.`)
+                    .setColor(this.client.colors.success)
+            );
+
+            async function BigPurge(messageCount) {
+                const messagesToPurge = messageCount > 99 ? 99 : messageCount;
+
+                message.channel.bulkDelete(messagesToPurge);
+
+                //console.log(`Clearing ${messagesToPurge}`)
+
+                if (messageCount > 99) {
+                    await sleep(2500);
+                    return BigPurge(messageCount - 99);
+                } else {
+                    message.channel
+                        .send(purgedembed)
+                        .then((msg) => msg.delete({ timeout: 5000 }));
+                }
+            }
+
+            function sleep(ms) {
+                return new Promise((resolve) => setTimeout(resolve, ms));
+            }
+        } catch (err) {
+            this.client.utils.handleError(err, message);
+        }
+    }
+};
