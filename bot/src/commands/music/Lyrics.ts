8284bc2559d367c09a82e4d7f22d99158f8962e7
Beatzoid <epiccoder7@gmail.com>
2021-01-23 14:48:39 -0800

    ✨ Add Lyrics Command
    lyrics

---
+++ bot/src/commands/music/Lyrics.ts
@@ -0,0 +1,23 @@
+import { Message } from "discord.js";
+import { CustomCommand } from "../../classes/Command";
+
+export default class LyricsCommand extends CustomCommand {
+    public constructor() {
+        super("lyrics", {
+            aliases: ["lyrics"],
+            category: "Music",
+            description: {
+                content: "Get the lyrics of the currently playing song",
+                usage: "lyrics",
+                examples: ["lyrics"]
+            },
+            ratelimit: 3
+        });
+    }
+
+    public exec(message: Message): Promise<Message> {
+        return message.util.send(
+            "This is one of the commands I need ksoft for"
+        );
+    }
+}
