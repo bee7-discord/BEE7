11962e7d7362e6cb3170c0b118eadc9b8deb9af3
Beatzoid <epiccoder7@gmail.com>
2021-01-31 18:52:15 -0800

    ✨ Add Clear Warns Command
    Add a command that allows a user with admin permissions to clear all of a users warns

---
+++ bot/src/commands/moderation/ClearWarns.ts
@@ -0,0 +1,62 @@
+import { User } from "discord.js";
+import { Message } from "discord.js";
+import { CustomCommand } from "../../classes/Command";
+import Warns from "../../models/Warn";
+
+export default class PingCommand extends CustomCommand {
+    public constructor() {
+        super("clearwarns", {
+            aliases: ["clearwarns", "cw"],
+            category: "Moderation",
+            description: {
+                content: "Clear ***all*** of a users warns",
+                usage: "clearwarns <user id or mention>",
+                examples: [
+                    "clearwarns <@444655632424108032>",
+                    "clearwarns 444655632424108032"
+                ]
+            },
+            args: [
+                {
+                    id: "member",
+                    type: "user",
+                    default: null
+                }
+            ],
+            userPermissions: ["ADMINISTRATOR"],
+            ratelimit: 3
+        });
+    }
+
+    public async exec(
+        message: Message,
+        { member }: { member: User }
+    ): Promise<Message> {
+        if (!member)
+            return message.channel.send(
+                "Incorrect usage or unknown user! | `clearwarns <member mention or id>`"
+            );
+
+        let existingWarns: any = await Warns.findOne({
+            guildId: message.guild.id
+        }).exec();
+
+        if (!existingWarns)
+            existingWarns = await Warns.create({ guildId: message.guild.id });
+
+        const userWarns = existingWarns.warns.filter(
+            (warn: any) => warn.user === member.id
+        );
+
+        if (!userWarns.length)
+            return message.channel.send("This user doesn't have any warns!");
+
+        existingWarns.warns.length = 0;
+        existingWarns.markModified("warns");
+        await existingWarns.save();
+
+        message.channel.send(
+            `Successfully cleared all warns from ${member.tag}`
+        );
+    }
+}
