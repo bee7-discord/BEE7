e86ba147bfceb5a937f6d7ea7104be8332d04192
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-03-21 10:02:09 -0700

    fix(Config): remove token from config

--- bot/src/Config.ts
+++ bot/src/Config.ts
@@ -2,15 +2,13 @@ import { BotOptions, PublicConfig as PubConfig } from "./utils/types";
 
 export const Config: BotOptions = {
     // The bot token
-    token: "ODAyMDE5ODY2NTI1Njk2MDUw.YApJNA.4Hk6Jh7ejZAh-3z4nUtRZspgYxA",
+    token: "",
     // The bots prefix you want to use
     prefix: "?",
     // Put your owner id (and maybe other people's as well)
-    owners: ["444655632424108032"],
+    owners: [""],
     // This only determines if the Mongo database is production or development
     devMode: true,
-    // If you don't want to get this, just remove the usage of it from Eval.ts
-    snowflakeApiKey: "",
 };
 
 export const PublicConfig: PubConfig = {
